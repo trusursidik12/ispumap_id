96a0a9aff3c30e0c2226013ca2a19e978dd83766
warih suryono <warih_suryono@yahoo.com>
2020-09-14 07:56:51 +0700

    aaa

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -26,44 +26,44 @@ class Home extends BaseController
 		// $header_provinces[7]["id_stasiun"] = "KOTAWARINGIN_BARAT";
 		// $header_provinces[8]["name"] = "Kalimantan Timur";
 		// $header_provinces[8]["id_stasiun"] = "BALIKPAPAN_BB";
-		foreach ($header_provinces as $key => $province) {
-			$ch = curl_init(API_URL . "aqmprovince?trusur_api_key=" . API_KEY . "&provinsi=" . urlencode($province["name"]));
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-			$output = curl_exec($ch);
-			curl_close($ch);
-			if (stripos(" " . $output, "\"status\":true") > 0) {
-				$aqmprovince = json_decode("[" . $output . "]")[0];
-				if ($aqmprovince->resumes->worst_stasiun_id != "")
-					$stasiun_id = str_replace("KLHK-", "", $aqmprovince->resumes->worst_stasiun_id);
-				else
-					$stasiun_id = $province["id_stasiun"];
+		// foreach ($header_provinces as $key => $province) {
+		// 	$ch = curl_init(API_URL . "aqmprovince?trusur_api_key=" . API_KEY . "&provinsi=" . urlencode($province["name"]));
+		// 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		// 	$output = curl_exec($ch);
+		// 	curl_close($ch);
+		// 	if (stripos(" " . $output, "\"status\":true") > 0) {
+		// 		$aqmprovince = json_decode("[" . $output . "]")[0];
+		// 		if ($aqmprovince->resumes->worst_stasiun_id != "")
+		// 			$stasiun_id = str_replace("KLHK-", "", $aqmprovince->resumes->worst_stasiun_id);
+		// 		else
+		// 			$stasiun_id = $province["id_stasiun"];
 
-				$header_provinces[$key]["category"] = $aqmprovince->resumes->category;
-				foreach ($aqmprovince->resumes->ispu as $ispu_s) {
-					if ($ispu_s->id_stasiun == $stasiun_id || $ispu_s->id_stasiun == "KLHK-" . $stasiun_id) {
-						$ispu_s->btn_class_pm10 = $this->getCssIspuCategory($ispu_s->pm10);
-						$ispu_s->btn_class_pm25 = $this->getCssIspuCategory($ispu_s->pm25);
-						$ispu_s->btn_class_o3 = $this->getCssIspuCategory($ispu_s->o3);
-						$ispu_s->btn_class_so2 = $this->getCssIspuCategory($ispu_s->so2);
-						$ispu_s->btn_class_no2 = $this->getCssIspuCategory($ispu_s->no2);
-						$ispu_s->btn_class_co = $this->getCssIspuCategory($ispu_s->co);
-						$header_provinces[$key]["ispu"] = $ispu_s;
-						break;
-					}
-				}
+		// 		$header_provinces[$key]["category"] = $aqmprovince->resumes->category;
+		// 		foreach ($aqmprovince->resumes->ispu as $ispu_s) {
+		// 			if ($ispu_s->id_stasiun == $stasiun_id || $ispu_s->id_stasiun == "KLHK-" . $stasiun_id) {
+		// 				$ispu_s->btn_class_pm10 = $this->getCssIspuCategory($ispu_s->pm10);
+		// 				$ispu_s->btn_class_pm25 = $this->getCssIspuCategory($ispu_s->pm25);
+		// 				$ispu_s->btn_class_o3 = $this->getCssIspuCategory($ispu_s->o3);
+		// 				$ispu_s->btn_class_so2 = $this->getCssIspuCategory($ispu_s->so2);
+		// 				$ispu_s->btn_class_no2 = $this->getCssIspuCategory($ispu_s->no2);
+		// 				$ispu_s->btn_class_co = $this->getCssIspuCategory($ispu_s->co);
+		// 				$header_provinces[$key]["ispu"] = $ispu_s;
+		// 				break;
+		// 			}
+		// 		}
 
-				$ch = curl_init(API_URL . "aqmdata?trusur_api_key=" . API_KEY . "&id_stasiun=" . $stasiun_id);
-				curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-				$output = curl_exec($ch);
-				curl_close($ch);
-				if (stripos(" " . $output, "\"status\":true") > 0) {
-					$aqmdata = json_decode("[" . $output . "]")[0];
-					$header_provinces[$key]["data"] = $aqmdata->data;
-				} else {
-					$header_provinces[$key]["data"] = NULL;
-				}
-			}
-		}
+		// 		$ch = curl_init(API_URL . "aqmdata?trusur_api_key=" . API_KEY . "&id_stasiun=" . $stasiun_id);
+		// 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		// 		$output = curl_exec($ch);
+		// 		curl_close($ch);
+		// 		if (stripos(" " . $output, "\"status\":true") > 0) {
+		// 			$aqmdata = json_decode("[" . $output . "]")[0];
+		// 			$header_provinces[$key]["data"] = $aqmdata->data;
+		// 		} else {
+		// 			$header_provinces[$key]["data"] = NULL;
+		// 		}
+		// 	}
+		// }
 
 		$data["header_provinces"] = $header_provinces;
 		$googlemaps->initialize();
@@ -74,9 +74,9 @@ class Home extends BaseController
 		$data["map"] = $googlemaps->create_map();
 		echo view('v_header', $data);
 		echo view('v_menu');
-		echo view('v_home');
+		// echo view('v_home');
 		echo view('v_footer');
-		echo view('v_home_js');
+		// echo view('v_home_js');
 	}
 
 	//--------------------------------------------------------------------
