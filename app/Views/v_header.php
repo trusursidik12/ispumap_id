da8d59aebf1b4063f4a39cf468eab5546836fc2a
warih suryono <warih_suryono@yahoo.com>
2020-11-10 08:26:35 +0700

    aaa

--- app/Views/v_header.php
+++ app/Views/v_header.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -13,9 +14,12 @@
 
     <link rel="stylesheet" href="<?= base_url(); ?>/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?= base_url(); ?>/css/font-awesome.min.css">
-    <link rel="stylesheet" href="<?= base_url(); ?>/css/leaflet.css" integrity="<?= LEAFLET_CSS_INTEGRITY ?>" crossorigin="" />
-    <script src="<?= base_url(); ?>/js/leaflet.js" integrity="<?= LEAFLET_JS_INTEGRITY ?>" crossorigin=""></script>
-    <script src="<?= base_url(); ?>/js/plotly-latest.min.js"></script>
+    <!--leaflet CSS-->
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin="" />
+    <!--leaflet JS; note to self; this must be after the css-->
+    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin="">
+        < script src = "<?= base_url(); ?>/js/plotly-latest.min.js" >
+    </script>
     <link rel="stylesheet" href="<?= base_url(); ?>/css/ispumaps_stylesheet.css">
     <style>
         .ispu-form .form-control {
